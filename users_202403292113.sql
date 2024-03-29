9a21faa8a3a94979d21cb35375221fbc85825547
hieubuict96 <dinhhieu.bui@migitek.com>
2024-03-29 21:15:25 +0700

    fthf

---
+++ users_202403292113.sql
@@ -0,0 +1,5 @@
+INSERT INTO project.users (phone,password,email,img_url,full_name,created_time) VALUES
+	 ('hieubuict96','$2a$10$6BYOHKOqYGoMgtGz9Nv27ectSXoU5uZ0D4jyqpMYxLuh.V8X5LIq.','hieubuict96@gmail.com','1-1711296353398.jpg','Bùi Đình Hiếu','2024-03-24 22:58:14'),
+	 ('hungbui94','$2a$10$..oItILRfDx6J6LjC76Bg..a6aB67v8S6AzPG4gAxhzcD3vQ0k9CO','hungbui94@gmail.com','2-1711296560446.jpg','Bùi Đình Hùng','2024-03-24 23:09:20'),
+	 ('tienson89','$2a$10$t24.sRAqvaJjDKqXwqdNOORey3Qbk5vVfXXUxjk0AikZ9Jhs/Ya4O','tienson89@gmail.com','3-1711296689400.jpg','Bùi Tiến Sơn','2024-03-24 23:11:29'),
+	 ('phuongbui','$2a$10$t24.sRAqvaJjDKqXwqdNOORey3Qbk5vVfXXUxjk0AikZ9Jhs/Ya4O','phuongbui@gmail.com','4-1711296689400.jpg','Bùi Lê Phương','2024-03-24 23:11:29');
