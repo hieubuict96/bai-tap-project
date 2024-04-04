6ef5144ee0be9fbfab7b91c4199165267fe1039d
hieubuict96 <dinhhieu.bui@migitek.com>
2024-04-04 13:23:34 +0700

    g

---
+++ group_chat_202404041322.sql
@@ -0,0 +1,4 @@
+INSERT INTO project.group_chat (name,user_id_admin,img_url,created_time,is_2_person) VALUES
+	 (NULL,NULL,NULL,'2024-03-28 08:27:35',1),
+	 ('sd',2,NULL,'2024-03-28 08:27:35',0),
+	 ('sdd',2,NULL,'2024-03-28 08:27:35',0);
---
+++ members_of_group_202404041322.sql
@@ -0,0 +1,11 @@
+INSERT INTO project.members_of_group (user_id,group_id) VALUES
+	 (1,1),
+	 (1,2),
+	 (1,3),
+	 (2,1),
+	 (2,2),
+	 (2,3),
+	 (3,2),
+	 (3,3),
+	 (4,2),
+	 (4,3);
