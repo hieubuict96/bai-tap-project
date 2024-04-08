c00e53128cf6f3add64e24f9e02465c98ec1ab8c
hieubuict96 <dinhhieu.bui@migitek.com>
2024-04-08 08:57:21 +0700

    Revert "gsdfg"
    This reverts commit 9cc209ddf1cf4a31d571a6de06a09b940b6c8938.

--- comments_202404041322.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO project.comments (post_id,user_id,content,created_time) VALUES
-	 (1,1,'bai hay qua1','2024-04-04 06:45:34'),
-	 (1,1,'bai hay qua7','2024-04-04 06:45:39'),
-	 (1,1,'bai hay qua6','2024-04-04 06:45:38'),
-	 (1,1,'bai hay qua2','2024-04-04 06:45:34'),
-	 (1,1,'bai hay qua3','2024-04-04 06:45:35'),
-	 (1,2,'bai hay qua5','2024-04-04 06:45:37'),
-	 (1,2,'bai hay qua8','2024-04-04 06:45:40'),
-	 (1,2,'bai hay qua4','2024-04-04 06:45:36');
