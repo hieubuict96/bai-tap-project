36203791501d0d50e4cf1644c8f2c00c6136475e
hieubuict96 <dinhhieu.bui@migitek.com>
2024-04-08 08:53:32 +0700

    Revert "xoa"
    This reverts commit b5c6747d52782be040efba064b6bfa63fdfe8464.

---
+++ posts_202404041322.sql
@@ -0,0 +1,5 @@
+INSERT INTO project.posts (user_id,content,created_time) VALUES
+	 (1,'this is content1',NULL),
+	 (1,'this is content2',NULL),
+	 (1,'this is content3',NULL),
+	 (1,'this is content4',NULL);
