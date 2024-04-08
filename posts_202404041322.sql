badfdf2a6c1e555a031a7ba446d3be938a61db75
hieubuict96 <dinhhieu.bui@migitek.com>
2024-04-08 07:27:59 +0700

    fd

--- posts_202404041322.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO project.posts (user_id,content,created_time) VALUES
-	 (1,'this is content1',NULL),
-	 (1,'this is content2',NULL),
-	 (1,'this is content3',NULL),
-	 (1,'this is content4',NULL);
